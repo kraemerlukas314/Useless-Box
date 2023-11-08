5909c370f48929f24b70fcc554c55165aba1cff5
Fulcrum <dragon.fighter@outlook.de>
2023-11-08 09:39:21 +0100

    Display update

--- LCD_1inch28/LCD_1inch28.ino
+++ LCD_1inch28/LCD_1inch28.ino
@@ -11,30 +11,83 @@
 // Hardware SPI on Feather or other boards
 Adafruit_GC9A01A tft(TFT_CS, TFT_DC);
 const int BACKGROUND = BLACK;
+const int MOUTH_COLOR = WHITE;
+const int X_MIN = 0;
+const int Y_MIN = 0;
+const int X_MAX = 240;
+const int Y_MAX = 240;
+const int Y_MOUTH_START = Y_MAX / 2.4;
+const int MOUTH_THICKNESS = 15;
+const int EYES_DISTANCE_EDGES = 30;
+const int EYES_SIZE = 30;
+
+int eyesColor = BLUE;
+
 
 void setup() {
-  
+
+
 
   tft.begin();
   tft.fillScreen(BLACK);
-  tft.fillCircle(120, 120, 100, WHITE);
-  tft.fillRect(0, 0, 240, 120, BLACK);
-  tft.fillCircle(120, 105, 90, BLACK);
-  
+  eyesNeutral();
+  mouthNeutral();
 }
 
-void clearDisplay() {
-  tft.fillScreen(BACKGROUND)
+void displayClear() {
+  //Clear Display (Set display to background color)
+  tft.fillScreen(BACKGROUND);
 }
 
-void smile1(bool clearDisplay) {
-  if(clearDisplay) {
-    clearDisplay()
-  }
+void mouthReset() {
+  //Draw a Rectangle from start of mouth to end of display
+  tft.fillRect(X_MIN, Y_MOUTH_START, X_MAX, Y_MAX - Y_MOUTH_START, BACKGROUND);
+}
 
+void mouthSmile1() {
+  mouthReset();
+  tft.fillCircle(X_MAX / 2, Y_MAX / 2, Y_MOUTH_START, MOUTH_COLOR);
+  tft.fillRect(X_MIN, Y_MOUTH_START, X_MAX, Y_MAX / 2 - Y_MOUTH_START, BACKGROUND);
+  tft.fillCircle(X_MAX / 2, Y_MAX / 2 - MOUTH_THICKNESS, 90, BACKGROUND);
+}
+
+void mouthNeutral() {
+  mouthReset();
+  tft.drawLine(X_MAX * 0.125, Y_MAX * 0.625, X_MAX - (X_MAX * 0.125), Y_MAX * 0.625 + MOUTH_THICKNESS, MOUTH_COLOR);
+}
+
+void eyeLeftReset() {
+  tft.fillRect(X_MIN, Y_MIN, X_MAX / 2, Y_MOUTH_START, BACKGROUND);
+}
+
+void eyeRightReset() {
+  tft.fillRect(X_MAX / 2, Y_MIN, X_MAX / 2, Y_MOUTH_START, BACKGROUND);
+}
+
+void eyeLeftNeutral() {
+  eyeLeftReset();
+  tft.fillCircle(EYES_DISTANCE_EDGES + EYES_SIZE, EYES_DISTANCE_EDGES + EYES_SIZE, EYES_SIZE, WHITE);
+  tft.fillCircle(EYES_DISTANCE_EDGES + EYES_SIZE, EYES_DISTANCE_EDGES + EYES_SIZE, EYES_SIZE / 3, eyesColor);
+  tft.fillCircle(EYES_DISTANCE_EDGES + EYES_SIZE, EYES_DISTANCE_EDGES + EYES_SIZE, EYES_SIZE / 4, BLACK);
+}
+
+void eyeRightNeutral() {
+  eyeLeftReset();
+  tft.fillCircle(X_MAX - (EYES_DISTANCE_EDGES + EYES_SIZE), EYES_DISTANCE_EDGES + EYES_SIZE, EYES_SIZE, WHITE);
+  tft.fillCircle(X_MAX - (EYES_DISTANCE_EDGES + EYES_SIZE), EYES_DISTANCE_EDGES + EYES_SIZE, EYES_SIZE / 3, eyesColor);
+  tft.fillCircle(X_MAX - (EYES_DISTANCE_EDGES + EYES_SIZE), EYES_DISTANCE_EDGES + EYES_SIZE, EYES_SIZE / 4, BLACK);
+}
+
+void eyesNeutral() {
+  eyeLeftNeutral();
+  eyeRightNeutral();
+}
+
+void eyesNormal() {
 }
 void loop() {
-  smile1(true);
+  
+
 }
 
 
