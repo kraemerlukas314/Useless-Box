fbd9e3b62c4c244f29469c50ff6d079a95d06d37
kraemerlukas314 <lukas.kraemer.2004@gmail.com>
2023-11-22 12:01:39 +0100

    save button presses to eeprom

--- LCD_1inch28/LCD_1inch28.ino
+++ LCD_1inch28/LCD_1inch28.ino
@@ -1,4 +1,6 @@
 #include "defines.h"
+#include <EEPROM.h>
+
 
 // Hardware SPI on Feather or other boards
 // Adafruit_GC9A01A tft(TFT_CS, TFT_DC);
@@ -6,18 +8,39 @@
 // const int BLACK = 0x000000;
 // const int BACKGROUND = BLACK;
 
+uint32_t totalButtonPresses;
+
 void setup() {
   Serial.begin(115200);
+  totalButtonPresses = EEPROM.read(0) | (EEPROM.read(1) << 8) | (EEPROM.read(2) << 16); // get total button presses from EEPROM
+  Serial.print("Total button presses read from EEPROM: ");
+  Serial.println(totalButtonPresses);
   tft.begin();
   tft.fillScreen(BACKGROUND);
   pinMode(PIN_BTN, INPUT_PULLUP);
+
+  while (!getButtonState) {
+    // ANZEIGE: Bitte Schalter umlegen
+
+  }
+  // ANZEIGE: Insgesamt wurde der Schalter totalButtonPresses mal umgelegt
+  // ANZEIGE: Kalibrierung
 }
 
 void loop() {
+  
 }
 
 bool getButtonState() {
   return digitalRead(PIN_BTN);
+}
+
+// increments the total button counter and writes result to EEPROM
+void incrementTotalButtonCounter() {
+  ++totalButtonPresses;
+  EEPROM.write(0, totalButtonPresses & 0xFF);           // Write the low byte
+  EEPROM.write(1, (totalButtonPresses >> 8) & 0xFF);    // Write the middle byte
+  EEPROM.write(2, (totalButtonPresses >> 16) & 0xFF);   // Write the high byte
 }
 
 void displayClear() {
