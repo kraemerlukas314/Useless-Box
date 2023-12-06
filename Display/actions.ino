31109e2f1897de7b2cd9ed42be6de9afab4c2bb7
Fulcrum <dragon.fighter@outlook.de>
2023-12-06 12:04:47 +0100

    fixed errors

--- Display/actions.ino
+++ Display/actions.ino
@@ -6,18 +6,27 @@ void useSwitch()
     {
         mouthStatus = 0;
         eyesStatus = 0;
-        eyeLidsStatus = eyeLidsStandard;
+        eyeLidsStatus[0] = 3;
+        eyeLidsStatus[1] = 3;
+        eyeLidsStatus[2] = 3;
+        eyeLidsStatus[3] = 3;
     }
     else if (happiness > 100 && happiness <= 200)
     {
         mouthStatus = 1;
         eyesStatus = 0;
-        eyeLidsStatus = eyeLidsStandard;
+        eyeLidsStatus[0] = 3;
+        eyeLidsStatus[1] = 3;
+        eyeLidsStatus[2] = 3;
+        eyeLidsStatus[3] = 3;
     }
     else if (happiness > 200 && happiness <= 300)
     {
         mouthStatus = 2;
         eyesStatus = 0;
-        eyeLidsStatus = {5, 3, 3, 5};
+        eyeLidsStatus[0] = 5;
+        eyeLidsStatus[1] = 3;
+        eyeLidsStatus[2] = 3;
+        eyeLidsStatus[3] = 5;
     }
 }
