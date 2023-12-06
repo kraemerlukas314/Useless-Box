d7b15357af70ef9745c0ef9f19ad79b92369003b
Fulcrum <dragon.fighter@outlook.de>
2023-12-06 11:53:18 +0100

    emotionen in main implementiert

--- Display/actions.ino
+++ Display/actions.ino
@@ -2,4 +2,22 @@
 
 void useSwitch()
 {
+    if (happiness >= 0 && happiness <= 100)
+    {
+        mouthStatus = 0;
+        eyesStatus = 0;
+        eyeLidsStatus = eyeLidsStandard;
+    }
+    else if (happiness > 100 && happiness <= 200)
+    {
+        mouthStatus = 1;
+        eyesStatus = 0;
+        eyeLidsStatus = eyeLidsStandard;
+    }
+    else if (happiness > 200 && happiness <= 300)
+    {
+        mouthStatus = 2;
+        eyesStatus = 0;
+        eyeLidsStatus = {5, 3, 3, 5};
+    }
 }
--- Display/mouth.ino
+++ Display/mouth.ino
@@ -1,46 +1,59 @@
 #include "defines.h"
 
-void mouthReset() {
-  //Draw a Rectangle from start of mouth to end of display
+void mouthReset()
+{
+  // Draw a Rectangle from start of mouth to end of display
   tft.fillRect(X_MIN, Y_MOUTH_START, X_MAX, Y_MAX - Y_MOUTH_START, BACKGROUND);
 }
 
-void mouthSmile1() {
+void mouthSmile1()
+{
   mouthReset();
   tft.fillCircle(X_MAX / 2, Y_MAX / 2, Y_MOUTH_START, MOUTH_COLOR);
   tft.fillRect(X_MIN, Y_MOUTH_START, X_MAX, Y_MAX / 2 - Y_MOUTH_START, BACKGROUND);
   tft.fillCircle(X_MAX / 2, Y_MAX / 2 - MOUTH_THICKNESS, 90, BACKGROUND);
 }
 
-void mouthNeutral() {
+void mouthNeutral()
+{
   tft.fillRoundRect(50, MOUTH_MID - (MOUTH_THICKNESS / 2), 140, MOUTH_THICKNESS, 30, WHITE);
 }
 
-void mouthPositive1() {
+void mouthPositive1()
+{
   tft.fillCircle(X_MAX / 2, Y_MAX * 0.542, X_MAX * 0.75, MOUTH_COLOR);
   tft.fillCircle(X_MAX / 2, Y_MAX * 0.375, X_MAX * 0.792, BACKGROUND);
 }
 
-void mouthPositive2() {
+void mouthPositive2()
+{
   tft.fillCircle(X_MAX / 2, Y_MAX * 0.625, X_MAX * 0.292, MOUTH_COLOR);
   tft.fillCircle(X_MAX / 2, Y_MAX / 2, X_MAX / 3, BACKGROUND);
 }
 
-void mouthDrawStatus(int aStatus) {
+void mouthDrawStatus(int aStatus)
+{
   mouthReset();
-  switch (aStatus) {
-    case (0): mouthNeutral();
-    case (1): {
-        mouthPositive1();
-        eyesDrawStatus();
-      }
-    case (2): {
-        mouthPositive2();
-        eyesDrawStatus();
-      }
+  switch (aStatus)
+  {
+  case (0):
+    mouthNeutral();
+  case (1):
+  {
+    mouthPositive1();
+    eyesDrawStatus();
+    break;
+  }
+  case (2):
+  {
+    mouthPositive2();
+    eyesDrawStatus();
+    break;
+  }
   }
 }
 
-void mouthDrawStatus() {
+void mouthDrawStatus()
+{
   mouthDrawStatus(mouthStatus);
 }
